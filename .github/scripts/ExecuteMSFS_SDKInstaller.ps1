81511976bad2fc334442506499747bb4e6d5cc55
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2024-01-16 12:19:38 +0100

    Output MSFS SDK install directory in GitHub actions workflow

--- .github/scripts/ExecuteMSFS_SDKInstaller.ps1
+++ .github/scripts/ExecuteMSFS_SDKInstaller.ps1
@@ -9,4 +9,9 @@ $MsiExecProcess = Start-Process $MsiExecCmd `
     -NoNewWindow -Wait `
     -Verbose:($VerbosePreference -ne [System.Management.Automation.ActionPreference]::SilentlyContinue)
 Get-Content ".\msi\MSFS_SDK_Core_Installer.log" | Write-Host
+[ValidateNotNullOrEmpty()][string]$PostInstallMsFsSdkVariable = [System.Environment]::GetEnvironmentVariable(
+    "MSFS_SDK",
+    [System.EnvironmentVariableTarget]::Machine
+)
+"MSFS_SDK=${PostInstallMsFsSdkVariable}" | Out-File -Append $ENV:GITHUB_OUTPUT
 exit $MsiExecProcess.ExitCode

e63e7dbcfba8ed66d0664a0228147d69b1364fc7
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2024-01-16 12:11:17 +0100

    Create separate generate ABI workflow

---
+++ .github/scripts/CopySimConnectHeader.ps1
@@ -0,0 +1,13 @@
+[CmdletBinding()]
+param ()
+$HeaderFileName = "SimConnect.h"
+$TargetFolderPath = Join-Path -Resolve "abi" "SimConnect"
+$TargetFilePath = Join-Path $TargetFolderPath $HeaderFileName
+
+"#include <Windows.h>" | Out-File $TargetFilePath -Force -Encoding utf8NoBOM
+[ValidateNotNullOrEmpty()][string]$MsfsSdkPath = $ENV:MSFS_SDK
+$SimConnectSdkRootPath = Join-Path -Resolve $MsfsSdkPath "SimConnect SDK"
+$SimConnectLibPath = Join-Path $SimConnectSdkRootPath "lib"
+$SimConnectHeaderFile = Join-Path $SimConnectLibPath $HeaderFileName
+
+Get-Content $SimConnectHeaderFile | Out-File -Append $TargetFilePath -Encoding utf8NoBOM
--- .github/scripts/ExecuteMSFS_SDKInstaller.ps1
+++ .github/scripts/ExecuteMSFS_SDKInstaller.ps1
@@ -4,8 +4,9 @@ param ()
 $MsiExecCmd = Get-Command msiexec -ErrorAction Stop
 $MsiPath = Resolve-Path ".\msi\MSFS_SDK_Core_Installer.msi"
 $MsiLogFile = Join-Path "msi" "MSFS_SDK_Core_Installer.log"
-Start-Process $MsiExecCmd `
+$MsiExecProcess = Start-Process $MsiExecCmd `
     "/i", $MsiPath, "/quiet", "/l*", $MsiLogFile `
     -NoNewWindow -Wait `
     -Verbose:($VerbosePreference -ne [System.Management.Automation.ActionPreference]::SilentlyContinue)
 Get-Content ".\msi\MSFS_SDK_Core_Installer.log" | Write-Host
+exit $MsiExecProcess.ExitCode
